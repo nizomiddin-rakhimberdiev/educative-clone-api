d198ed6f4a0b62dc386286cd9d34c163aaae5ca9
Nizomiddin Rakhimberdiev <ranizomiddin@gmail.com>
2022-05-05 12:18:14 +0500

    changed api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,8 @@ from api.views import CourseViewSet, CourseReviewViewSet, UserViewSet
 app_name = 'api'
 
 router = DefaultRouter()
-router.register('course', CourseViewSet, basename='course'),
-router.register('review', CourseReviewViewSet, basename='review'),
+router.register('courses', CourseViewSet, basename='course'),
+router.register('reviews', CourseReviewViewSet, basename='review'),
 router.register('users', UserViewSet, basename='user'),
 
 urlpatterns = router.urls
